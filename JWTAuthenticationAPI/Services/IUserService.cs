5870a138fe8f6c72959c653bf10bcbffa262f06d
Akash <akashveer98@gmai.com>
2021-04-03 12:59:46 +0530

    Add project files.

---
+++ JWTAuthenticationAPI/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTAuthenticationAPI.Entities;
+using JWTAuthenticationAPI.Models;
+
+namespace JWTAuthenticationAPI.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
